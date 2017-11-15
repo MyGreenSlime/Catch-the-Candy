e511358dda4a0da2c2034e53e7d852e30666722b
DONDON <ruksachai.o@ku.th>
2017-11-15 17:53:52 +0700

    character can move but not good

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.game.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 400;
+		config.height = 700;
 		new LwjglApplication(new MyGame(), config);
 	}
 }
